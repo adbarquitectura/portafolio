98863ffd6689bcc2f5f72071f8bbcc9f1f360a2f
Amanda D <adnamadiaz@gmail.com>
2020-11-07 00:06:45 -0300

    Add new data to Data-projects

--- src/Data-proyectos.js
+++ src/Data-proyectos.js
@@ -4,11 +4,11 @@ export default{
         {
             "id": 1,
             "name": "Comida para todos",
-            "description": "Landing page para fundacion Comida para todos",            
+            "description": "Propuesta de diseño de landing page para fundacion Comida para todos",            
             "repository":"https://github.com/adbarquitectura/SCL014-comida-para-todos.git",
             "url": "https://comida-para-todos-page.web.app/",
             "img": "./assets/images/logos/Logo.png",
-            "observations": "Es un proyecto desarrollado para un cliente real y en combinacion con el equipo",
+            "observations": "Es un proyecto desarrollado para un cliente real, orientado bajo de diseño UX.",
             "tecnologies": [
                 {
                     "nameTecnology": "JavaScript",
@@ -23,7 +23,11 @@ export default{
                     "icon": "./assets/images/logos/Logo.png"
                 },
                 {
-                    "nameTecnology": "desktop",
+                    "nameTecnology": "Bootstrap",
+                    "icon": "./assets/images/logos/Logo.png"
+                },
+                {
+                    "nameTecnology": "Desktop",
                     "icon": "./assets/images/logos/Logo.png"
                 }
             ]            
@@ -31,11 +35,11 @@ export default{
         {
             "id": 2,
             "name": "Burger Song",
-            "description": "Aplicación desarrollada para registrar y gestionar pedidos de clientes de un restaurante.",            
-            "repository":"https://github.com/adbarquitectura/SCL014-comida-para-todos.git",
-            "url": "https://comida-para-todos-page.web.app/",
+            "description": "Aplicación desarrollada para registrar y gestionar pedidos de clientes de un restaurante, inspirada en una temática personalizada para cada usuario, que permite almacenar y controlar los pedidos en todo el proceso.",            
+            "repository":"https://github.com/adbarquitectura/burger-queen.git",
+            "url": "https://burgersong.on.fleek.co/waiter",
             "img": "./assets/images/data-projects/projComida.png",
-            "observations": "Permite al usurio personalizar su pedido",
+            "observations": "Permite al usurio personalizar su pedido.",
             "tecnologies": [
                 {
                     "nameTecnology": "JavaScript",
@@ -50,7 +54,49 @@ export default{
                     "icon": "./assets/images/logos/Logo.png"
                 },
                 {
-                    "nameTecnology": "tablet",
+                    "nameTecnology": "Tablet",
+                    "icon": "./assets/images/logos/Logo.png"
+                }
+            ]            
+        },
+        {
+            "id": 3,
+            "name": "Nómada",
+            "description": "Red social desarrollada para viajeros y aventureros, donde podrán compartir, comentar, editar, interactuar y almacenar sus experiencias de viaje y conectar con otros, fomentando la comunidad y el enriquecimiento cultural.",            
+            "repository":"https://github.com/adbarquitectura/SCL014-social-network.git",
+            "url": "https://naterayc.github.io/SCL014-social-network/#/",
+            "img": "./assets/images/data-projects/projComida.png",
+            "observations": "Permite al usurio personalizar su pedido.",
+            "tecnologies": [
+                {
+                    "nameTecnology": "JavaScript",
+                    "icon": "./assets/images/logos/Logo.png"
+                },                
+                {
+                    "nameTecnology": "Firebase",
+                    "icon": "./assets/images/logos/Logo.png"
+                },
+                {
+                    "nameTecnology": "Responsive",
+                    "icon": "./assets/images/logos/Logo.png"
+                }
+            ]            
+        },
+        {
+            "id": 4,
+            "name": "Pokémon Godex",
+            "description": "Aplicación inspirada en el juego Pokémon Go, desarrollada para usuarios interesados en el entendimiento de las características de cada uno de sus Pokémon, a través del manejo de Data visualization.",            
+            "repository":"https://github.com/adbarquitectura/SCL014-data-lovers.git",
+            "url": "https://adbarquitectura.github.io/SCL014-data-lovers/",
+            "img": "./assets/images/data-projects/projComida.png",
+            "observations": "",
+            "tecnologies": [
+                {
+                    "nameTecnology": "JavaScript",
+                    "icon": "./assets/images/logos/Logo.png"
+                },                
+                {
+                    "nameTecnology": "Responsive",
                     "icon": "./assets/images/logos/Logo.png"
                 }
             ]            
--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -3,11 +3,20 @@ import ButtonPrimary from '../components/buttons/ButtonPrimary';
 import ButtonSecundary from '../components/buttons/ButtonSecundary';
 import NetworksSection from '../components/fixed-components/NetworksSection';
 import TitleMain from '../components/headers/TitleMain';
+import nexticon from '../assets/icons/nexticon.png';
 
 const HomeView = () =>{
     return(
         <div> 
             <TitleMain />
+            <p>
+            “En las soluciones tecnológicas  fusiono mis conocimientos 
+            en el área de programación, diseño y administración, generando 
+            propuestas de valor que intenten responder a las necesidades del 
+            futuro utilizando menos recursos.”
+            </p>
+            <h4>Conoce más de mí</h4>
+            <img src={nexticon} alt= ""/>
             <NetworksSection />
             
         </div>
