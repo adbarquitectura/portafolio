7351a0bad7c189db268aa4bf8a925ab3b6a4b288
Amanda D <adnamadiaz@gmail.com>
2020-11-13 12:55:20 -0300

    Update card project styles

--- src/components/dinamic-components/TextDescriptionItem.jsx
+++ src/components/dinamic-components/TextDescriptionItem.jsx
@@ -1,19 +1,24 @@
 import React, { useState } from 'react';
+import '../../views/ProjectsView.css';
 import ButtonText from '../buttons/ButtonText';
 
 const TextDescriptionItem = (props) => {
     const [styleBoxText, setStyleBoxText] = useState({ display: 'none' })
 
     const showText = () => {
-        setStyleBoxText({ display: 'block' });
+        setStyleBoxText({
+            display: 'block',
+            animationName: 'example',
+            animationDuration: '0.5s',
+        });
     }
 
     /* const hideText = () => {
         setStyleBoxText({ display: 'none' });
     } */
-    
+
     return (
-        <div>
+        <div className="box-text-description">
             <ButtonText
                 contenidobtn={props.nameProject}
                 funcion={showText}
