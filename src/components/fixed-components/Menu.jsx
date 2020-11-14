16d6062fd2e16ecf3feb1fee20065f0b63b74e12
Amanda D <adnamadiaz@gmail.com>
2020-11-13 23:05:20 -0300

    Changed contact component

--- src/components/fixed-components/Menu.jsx
+++ src/components/fixed-components/Menu.jsx
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Menu.css';
 import ButtonPrimary from '../buttons/ButtonPrimary';
 import ButtonSecundary from '../buttons/ButtonSecundary';
 import ButtonCircle from '../buttons/ButtonCircle';
 import whatsapp from '../../assets/icons/whatsapp.png';
+import ButtonText from '../buttons/ButtonText';
 
 const Menu = () => {
-    return (
-        <div className="box-menu">
+    const [textButtonCircle, setTextButtonCircle] = useState('+')
+    const [colorMenu, setColorMenu] = useState({ backgroundColor: '#021130' })
+    const [showMenu, setShowMenu,] = useState({ display: 'none' })
+    const [showContact, setShowContact,] = useState({ display: 'none' })
 
-            <div className="box-menu-button">
+    const showMenuList = () => {
+        setColorMenu({ backgroundColor: 'red' });
+        //setColorMenu({backgroundColor: '#003D5D'});
+        setTextButtonCircle('-');
+        setShowMenu({ display: 'block' });
+    };
+
+    const showMenuContact = () => {
+        setShowContact({ display: 'block' });
+    }
+
+    return (
+        <div style={colorMenu} className="box-menu">
+
+            <div style={colorMenu} className="box-menu-button">
                 <ButtonCircle
-                    contenidobtn='+'
+                    contenidobtn={textButtonCircle}
+                    funcion={showMenuList}
                 />
             </div>
 
-
-            <div className="box-menu-dropdown">
+            <div style={showMenu} className="box-menu-dropdown">
                 <ul>
                     <li>Acerca de mí</li>
                     <li>Proyectos</li>
                     <li>Contacto</li>
                 </ul>
+                
             </div>
-            <div className="box-contacto-section-not-visible">
+            <div style={showContact} className="box-contacto-section-not-visible">
                 <p>Puedes contactarme aquí.</p>
                 <ButtonSecundary
                     imgsrc={whatsapp}
