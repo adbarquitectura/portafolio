4a9db37aaa287e1cad747b34e0b1e23c9ae17815
Amanda D <adnamadiaz@gmail.com>
2020-11-19 13:26:20 -0300

    Add new styles for projects cards

--- src/views/ProjectsView.jsx
+++ src/views/ProjectsView.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './ProjectsView.css';
 import ButtonSecundaryLink from '../components/buttons/ButtonSecundaryLink';
-import  TextDescriptionItem from '../components/dinamic-components/TextDescriptionItem';
+import TextDescriptionItem from '../components/dinamic-components/TextDescriptionItem';
 
 const ProjectsView = () => {
 
     const [dataProject, setDataProject] = useState([]);
-  
+
     const getDataProject = () => {
         fetch('Data-proyectos.json')
             .then(response => response.json())
@@ -25,33 +25,37 @@ const ProjectsView = () => {
                 {
                     dataProject.map((project, index) => {
                         return (
-                            <div key={index} className="box-cards-single">
-                                
-                                <TextDescriptionItem
-                                    nameProject={project.name}
-                                    descriptionProject={project.description}
-                                />
-                               
-                                <div className="box-img-card">
-                                    <img src={project.img} alt={project.name} />
-                                </div>
-                             
-                     
-                                <div className="box-cards-tecnologies">
-                                    {project.tecnologies.map((tecnology, indx) => {
-                                        return (
-                                            <div key={indx} className="box-img-icon">
-                                                <img src={tecnology.icon} alt={tecnology.nameTecnology} />
-                                            </div>
-                                        )
+                            <div styles={{ backgroundImage: `url('${project.img}')` }} key={index} className="box-cards-single">
 
-                                    })}
+                                <div className="box-cards-in-single">
+                                    <div className="box-img-card">
+                                        <img src={project.img} alt={project.name} />
+                                    </div>
+
+                                    <TextDescriptionItem
+                                        nameProject={project.name}
+                                        descriptionProject={project.description}
+                                    />
+
+                                    <div className="box-cards-tecnologies">
+                                        {project.tecnologies.map((tecnology, indx) => {
+                                            return (
+                                                <div key={indx} className="box-img-icon">
+                                                    <img src={tecnology.icon} alt={tecnology.nameTecnology} />
+                                                </div>
+                                            )
+
+                                        })}
+                                    </div>
+
+                                    <ButtonSecundaryLink
+                                        anclaLinkTo={project.url}
+                                        contenidobtn=' Ir a la App'
+                                    />
                                 </div>
-                                <ButtonSecundaryLink
-                                    anclaLinkTo={project.url}
-                                    contenidobtn=' Ir a la App'
-                                />
-                                
+
+
+
                             </div>
 
                         )
