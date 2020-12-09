d8875104eb46793333bf15d296de7fe38bf95708
Amanda D <adnamadiaz@gmail.com>
2020-12-09 20:32:53 -0300

    Add new icon to networkSection component

--- src/components/fixed-components/NavBar.jsx
+++ src/components/fixed-components/NavBar.jsx
@@ -17,9 +17,9 @@ const NavBar = () => {
 
         <nav className={`box-navBar ${scrolled}`}>
             <ul className="box-menu-desktop" >
-                <li><a href="#proyectos">Proyectos</a></li>
+                {/* <li><a href="#proyectos">Proyectos</a></li> */}
                 <li><a href="#acerca">Acerca de mí</a></li>
-                <li><a href="#contanto">Contacto</a></li>
+                {/* <li><a href="#contanto">Contacto</a></li> */}
             </ul>
         </nav>
 
